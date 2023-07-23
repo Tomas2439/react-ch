5402775a7013f60edb264c08a3741f5f227073df
Tomas Stutz <tomasbautistastutz@gmail.com>
2023-07-22 21:49:16 -0300

    Update CartContext
    Actualización del CartContext.
    Cambios de Variables y de Sintaxis.

--- neco-ecommerce/src/context/CartContext.jsx
+++ neco-ecommerce/src/context/CartContext.jsx
@@ -1,36 +1,55 @@
 import { createContext, useState } from "react";
 
 export const CartContext = createContext({
-    cart: []
+    cart: [],
+    total: 0,
+    totalQuantity:0,
 })
 
 export const CartProvider = ({children}) => {
     const [cart, setCart] = useState([])
+    const [total, setTotal] = useState(0);
+    const [totalQuantity, setTotalQuantity] = useState(0);
 
     console.log(cart)
-
+    //funcion agregar producto de al carrito
     const addItem = (item, quantity) => {
+        const isInCart = cart.find(prod => prod.item.id === item.id);
+
         if(!isInCart(item.id)) {
-            setCart(prev => [...prev, {...item, quantity}])
+            setCart(prev => [...prev, {...item, quantity}]);
+            setTotalQuantity(prev => prev + quantity);
+            setTotal(prev => prev + (item.price * quantity));
         }else{
-            console.error('El producto fue agregado')
+            const cartUpdated = cart.map(prod => {
+                if(prod.item.id === item.id){
+                    return {...prod, quantity: prod.quantity + quantity};
+                }else{
+                    return prod;
+                }
+            });
+            setCart(cartUpdated);
+            setTotalQuantity(prev => prev + quantity);
+            setTotal(prev => prev + q)
         }
     }
-    
-    const removeItem = (itemId) => {
-        const cartUpdated = cart.filter(prod => prod.id !== itemId)
-        setCart(cartUpdated)
+    //funcion remover UN ITEM del carrito
+    const removeItem = (id) => {
+        const productRemoved = cart.find(prod => prod.item.id === id);
+        const cartUpdated = cart.filter(prod => prod.item.id !== id);
+        setCart(cartUpdated);
+        setTotalQuantity(prev => prev - productRemoved.quantity);
+        setTotal(prev => prev - (productRemoved.item.price * productRemoved.quantity));
     }
+    //funcion LIMPIAR TODO el carrito
     const clearCart = () =>{
-        setCart([])
-    }
-
-    const isInCart = (itemId) => {
-        return cart.some(prod => prod.id === itemId)
+        setCart([]);
+        setTotal(0);
+        setTotalQuantity(0);
     }
     
     return(
-        <CartContext.Provider value={{ cart, addItem, removeItem, clearCart}}>
+        <CartContext.Provider value={{ cart, addItem, removeItem, clearCart, total, totalQuantity}}>
             {children}
         </CartContext.Provider>
     )
